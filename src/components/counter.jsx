717014f28dd153b67e9556cf003bc5c71ded0476
DJ VOLATYL <djvolatyl@DJs-MacBook-Pro.local>
2023-02-24 06:49:03 +0300

    Added new files
    added new js files for Routing, edited everything according to w3

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,2 @@
 import React, { Component } from "react";
 
-class Counter extends Component {
-  state = { count: 1, tags: [] };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return this.state.tags.map((tag) => <ul><li key={tag}>{tag}</li></ul>);
-  }
-  render() {
-    return (
-      <div style={{ marginTop: 10 }}>
-        {this.renderTags()}
-      </div>
-    );
-  }
-}
-
-export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
-import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+//import "bootstrap/dist/css/bootstrap.css";
+//import Counter from "./components/counter";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Counter />);
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+  
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<App />);
